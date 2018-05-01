2d4196e9d31a2670f5d01116dfded9cf859229a9
Kitty Davies <kittyfdavies@gmail.com>
2018-05-01 23:54:28 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,14 @@
 require 'pry'
 class Post
   attr_accessor :name, :title, :author
-  
-  def initialize(title)
-    @title = title
+
+  def initialize(name)
+    @name = name
   end
-  
+
   def author_name
     binding.pry
     self.author.name
   end
-  
+
 end

86d733bf1e92678255ab8b59a4cb9451e7539891
Kitty Davies <kittyfdavies@gmail.com>
2018-05-02 00:11:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,17 @@
 require 'pry'
 class Song
   attr_accessor :artist, :name
+  
   def initialize(name)
     @name = name
   end
+
   def artist_name
-    artist_name = self.artist.name
-    if artist_name
+    if self.artist
       self.artist.name
     else
       nil
     end
   end
+  
 end
